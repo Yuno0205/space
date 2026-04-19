df6b3a3c6baa0a3084cce2c62acebaeb9db1de7f
Yuno0205 <mainhathao195@gmail.com>
2026-04-19 10:43:30 +0700

    fix: missing env var

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,12 @@
 import { createBrowserClient } from "@supabase/ssr";
 
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY) {
+  throw new Error("Missing environment variables");
+}
+
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+    String(process.env.NEXT_PUBLIC_SUPABASE_URL),
+    String(process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY)
   );
 }
